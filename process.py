0eb931acd0202251bfd6a400422069d4771d5526
odymit <15167656+odymit@users.noreply.github.com>
2023-10-15 21:21:34 +0800

    update data format

--- process.py
+++ process.py
@@ -7,20 +7,32 @@ from pathlib import Path
 def load_data(data_src_dir):
     failed_list = {}
     finished_list = {}
+    sample_filter_list = {}
     for item in data_src_dir.iterdir():
         if "finished" in item.as_posix():
             for file in item.iterdir():
+                filename = file.name.strip(file.suffix)
+                if "strip" in file.as_posix():
+                    with open(file, "r") as f:
+                        data = json.load(f)
+                        sample_filter_list[filename] = data
+                if "activation" in file.as_posix():
+                    continue
                 if ".log" not in file.as_posix():
                     data = {}
                     with open(file, "r") as f:
                         data = json.load(f)
                     results = {}
-                    results["c-acc"] = data[-1][f"test_clean_acc"]
+                    print(len(data))
+                    print(file)
+                    if len(data) < 3:
+                        results["c-acc"] = "-"
+                    else:
+                        results["c-acc"] = data[-1][f"test_clean_acc"]
                     results["b-acc"] = data[0][f"test_clean_acc/dataloader_idx_0"]
                     results["asr"] = data[1][f"test_asr/dataloader_idx_1"]
                     results["ra"] = data[1][f"test_ra/dataloader_idx_1"]
 
-                    filename = file.name.strip(file.suffix)
                     finished_list[filename] = results
         elif "failed" in item.as_posix():
             for file in item.iterdir():
@@ -28,19 +40,23 @@ def load_data(data_src_dir):
                 failed_list[filename] = "failed"
         else:
             pass
-    return failed_list, finished_list
-
-
-def get_format_data(finished_list):
+    return failed_list, finished_list, sample_filter_list
+def get_sf_data(sample_filter_list):
     data = []
     for filename in finished_list.keys():
-        data_type, attack, dataset, model, _, pratio = filename.split("-")
+        try:
+            data_type, attack, dataset, model, _, pratio = filename.split("-")
+            defense = "-"
+        except:
+            data_type, attack, dataset, model, _, pratio, defense = filename.split("-")
+
         c_acc, b_acc, asr, ra = itemgetter("c-acc", "b-acc", "asr", "ra")(
             finished_list[filename]
         )
         keys = [
             "data_type",
             "attack",
+            "defense",
             "dataset",
             "model",
             "pratio",
@@ -49,7 +65,61 @@ def get_format_data(finished_list):
             "asr",
             "ra",
         ]
-        values = [data_type, attack, dataset, model, pratio, c_acc, b_acc, asr, ra]
+        values = [
+            data_type,
+            attack,
+            defense,
+            dataset,
+            model,
+            pratio,
+            c_acc,
+            b_acc,
+            asr,
+            ra,
+        ]
+        cur_data = {}
+        for k, v in zip(keys, values):
+            cur_data[k] = v
+        data.append(cur_data)
+    return data
+
+
+def get_format_data(finished_list):
+    data = []
+    for filename in finished_list.keys():
+        try:
+            data_type, attack, dataset, model, _, pratio = filename.split("-")
+            defense = "-"
+        except:
+            data_type, attack, dataset, model, _, pratio, defense = filename.split("-")
+
+        c_acc, b_acc, asr, ra = itemgetter("c-acc", "b-acc", "asr", "ra")(
+            finished_list[filename]
+        )
+        keys = [
+            "data_type",
+            "attack",
+            "defense",
+            "dataset",
+            "model",
+            "pratio",
+            "c-acc",
+            "b-acc",
+            "asr",
+            "ra",
+        ]
+        values = [
+            data_type,
+            attack,
+            defense,
+            dataset,
+            model,
+            pratio,
+            c_acc,
+            b_acc,
+            asr,
+            ra,
+        ]
         cur_data = {}
         for k, v in zip(keys, values):
             cur_data[k] = v
@@ -69,9 +139,10 @@ def write2csv(data):
 
 if __name__ == "__main__":
     data_src_dir = Path(__file__).parent.parent / "dataset_collections"
-    failed_list, finished_list = load_data(data_src_dir)
+    failed_list, finished_list, sample_filter_list = load_data(data_src_dir)
     data = get_format_data(finished_list)
+    sf_data = get_sf_data(sample_filter_list)
     with open("data.json", "w", encoding="utf-8") as f:
         json.dump(data, f)
-    # write2csv(data)
+    write2csv(data)
     print("data saved in: ./data.csv")
